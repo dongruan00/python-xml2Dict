1056256c76ecf961f61db0229f75fcbff09c3e7b
Mc.Test <Test.Mc@gmail.com>
2014-01-03 13:25:58 +0800

    this is the first time

---
+++ practice.py
@@ -0,0 +1,28 @@
+﻿lists=[20,12,34,12,24,34,55,27]
+print list(set(lists))
+print sorted(set(lists),key=lists.index)
+
+print set('abc').intersection('cbs')
+
+
+s = set([3,5,9,10])
+t = set([3,6,7,10])
+##s.intersection_update(t)
+##print s,t
+##
+##s.update(t)
+##print s,t
+
+##s.difference_update(t)
+##print s,t
+
+s.symmetric_difference_update(t)
+print s,t
+
+
+theString = 'saaaay yes no yaaaass'
+print theString.strip('say') 
+print theString.strip('say ') #say后面有空格 
+print theString.lstrip('say') 
+print theString.rstrip('say') 
+
